955151108e3bc5811864528cd23ef63920942d25
shriyafxbytes <jain.shriya@fxbytes.com>
2019-01-02 10:45:03 +0530

    1. Code Optimisation

--- routes/web.php
+++ routes/web.php
@@ -30,45 +30,20 @@ Route::post('leaves/store', 'HomeController@postLeave')->name('leaves/store');
 Route::post('leaves/checkvalidation', 'HomeController@checkValidation')->name('leaves/checkvalidation');
 Route::get('leaves/showleaves', 'HomeController@showUserLeaveRecords')->name('leaves/showleaves');
 
-// Admin Login routes
+// Admin Routes
 Route::get('/admin', 'AdminController@getLogin');
 Route::post('/admin', 'AdminController@postLogin');
-
-// Admin Dashboard
+Route::post('/deleteUser', 'DashboardController@deleteUser');
 Route::group(['middleware' => ['admin']], function () {
     Route::get('/dashboard', ['as'=>'dashboard','uses'=>'DashboardController@index']);
-});
-// Admin users route
-Route::group(['middleware' => ['admin']], function () {
     Route::get('/users', ['as'=>'users','uses'=>'DashboardController@users']);
-});
-// Admin blogs route
-Route::group(['middleware' => ['admin']], function () {
     Route::get('/adminUsers', ['as'=>'dashboard','uses'=>'DashboardController@users']);
-});
-Route::post('/deleteUser', 'DashboardController@deleteUser');
-// Admin Add edit route
-Route::group(['middleware' => ['admin']], function () {
     Route::get('/addUser/{user_id?}', ['as'=>'addUser','uses'=>'DashboardController@getUser']);
-});
-Route::group(['middleware' => ['admin']], function () {
-    Route::post('/addUser/{user_id?}', ['as'=>'editUser','uses'=>'DashboardController@postUser']);
-});
-Route::group(['middleware' => ['admin']], function () {
     Route::get('/addLeave/{id}', ['as'=>'addLeave','uses'=>'DashboardController@getLeave']);
-});
-// Admin Dashboard
-Route::group(['middleware' => ['admin']], function () {
+    Route::post('/addUser/{user_id?}', ['as'=>'editUser','uses'=>'DashboardController@postUser']);
     Route::get('leaves/show/{id}', 'DashboardController@show')->name('leaves/show');
-});
-
-Route::group(['middleware' => ['admin']], function () {
     Route::post('admin/store', 'DashboardController@postLeave')->name('admin/store');
-});
-Route::group(['middleware' => ['admin']], function () {
     Route::post('admin/checkvalidation', 'DashboardController@checkValidation')->name('admin/checkvalidation');
-});
-Route::group(['middleware' => ['admin']], function () {
     Route::post('leaves/changestatus', 'DashboardController@changeLeaveStatus')->name('leaves/changestatus');
 });
 
